266c41e3d5fabd7f07c51d4649929a298ab496f8
catj0073@edu.zealand.dk <ZeaZEYVH!2>
2022-10-24 14:30:12 +0200

    Så burde koderne være der :-D

---
+++ andre_projekter.php
@@ -0,0 +1,42 @@
+<head>
+
+    <meta charset="utf-8">
+
+    <title> Susana Roma Buch </title>
+    <meta name="description" content="...">
+    <meta name="robots" content="All">
+    <meta name="author" content="Catja Kim Nielsen">
+    <meta name="copyright" content="Information om copyright">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta property="og:title" content="Susana Roma Buch">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://www.susanabuch.dk">
+    <meta property="og:image" content="...">
+    <meta property="og:description" content="...">
+    <meta property="og:locale" content="da.DK">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <style> body{background-color: rgb(240, 228, 226);} </style>
+    <style> a{color: rgb(169, 151, 110);}  a:link{color: rgb(169, 151, 110);}  a:hover{color: rgb(169, 151, 110);}</style>
+
+    <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed&family=Roboto:wght@300&family=Source+Sans+Pro:wght@300&display=swap" rel="stylesheet">
+
+</head>
+
+
+<body>
+
+    <?php include "includes/Navigation.php"; ?>
+
+    ...
+
+    <?php include "includes/Footer.php"; ?>
+
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
---
+++ includes/navigation.php
@@ -0,0 +1,25 @@
+
+<nav class="navbar navbar-expand-lg navbar-light w-100">
+
+    <div class="container-fluid">
+
+        <a class="navbar-brand ps-lg-5" href="index.php"> <img src="..." height="20px" width="100px" alt="Susana Roma Buch logo"> </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse w-100 justify-content-end pe-lg-5" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link roboto" aria-current="page" href="seo_og_marketing.php">SEO og marketing</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link roboto" aria-current="page" href="andre_projekter.php">Andre projekter</a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+
+</nav>
